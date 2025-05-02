5429a83ba747386c438ed6e62300fb97494dc1bc
Camilla Falcão <millafalcao.acad@gmail.com>
2025-05-01 22:18:32 -0300

    feat: add environment configuration for development

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  apiUrl: "https://accounts.spotify.com/api",
+  client: {
+    id: "bb1af51000784f2385e85f466ccfff5b",
+    secret: "e097da451d0546178c0bb36567ccdad6"
+  }
+};
