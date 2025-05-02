2f49f53ca8112efb28db7d9768cf6171779c10b2
Camilla Falcão <millafalcao.acad@gmail.com>
2025-05-02 06:27:43 -0300

    feat: add base component with basic layout structure

---
+++ src/app/pages/base/base.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AsideComponent } from '@shared/components/aside/aside/aside.component';
+import { HeaderComponent } from '@shared/components/header/header/header.component';
+
+@Component({
+  selector: 'app-base',
+  standalone: true,
+  imports: [CommonModule, RouterModule, AsideComponent, HeaderComponent],
+  templateUrl: './base.component.html',
+  styleUrls: ['./base.component.scss']
+})
+export class BaseComponent {
+
+}
