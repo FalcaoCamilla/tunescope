0868f1e3db369119c25ff285a080ccd5dae45131
Camilla Falcão <millafalcao.acad@gmail.com>
2025-05-01 22:21:11 -0300

    feat: add data models for access and user

---
+++ src/app/core/models/access-data-response.ts
@@ -0,0 +1,5 @@
+export type AccessDataResponse = {
+  access_token: string;
+  token_type: string;
+  expires_in: string;
+};
---
+++ src/app/core/models/index.ts
@@ -0,0 +1,2 @@
+export * from './access-data-response'
+export * from './user'
