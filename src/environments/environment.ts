516cb3e7ec4f9471432c79cb37690b9fe6476cd3
Camilla Falcão <millafalcao.acad@gmail.com>
2025-05-03 14:08:04 -0300

    fix: update authUrl and apiUrl for correct Spotify API endpoints

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: "https://accounts.spotify.com/api",
+  authUrl: "https://accounts.spotify.com/api/token",
+  apiUrl: "https://api.spotify.com/v1",
   client: {
     id: "bb1af51000784f2385e85f466ccfff5b",
     secret: "e097da451d0546178c0bb36567ccdad6"
