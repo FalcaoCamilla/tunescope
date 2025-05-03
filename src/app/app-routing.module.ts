8c82d7faac5ccbf2fa134344b2aae641ed26adc2
Camilla Falcão <millafalcao.acad@gmail.com>
2025-05-03 14:24:42 -0300

    feat: configure routing with authentication and component loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,55 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { authGuard } from '@core/guards/auth.guard';
+import { BaseComponent } from '@pages/base/base.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('@pages/login/login.component').then(m => m.LoginComponent),
+  },
+  {
+    path: '',
+    component: BaseComponent, 
+    canActivate: [authGuard],
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        loadComponent: () => import('@pages/home/home.component').then(m => m.HomeComponent),
+      },
+      {
+        path: 'albums',
+        loadComponent: () => import('@pages/albums/albums.component').then(m => m.AlbumsComponent),
+      },
+      {
+        path: 'albums/:id',
+        loadComponent: () => import('@pages/albums/album-details/album-details.component').then(m => m.AlbumDetailsComponent),
+      },
+      {
+        path: 'artists',
+        loadComponent: () => import('@pages/artists/artists.component').then(m => m.ArtistsComponent),
+      },
+      {
+        path: 'artist/:id',
+        loadComponent: () => import('@pages/artists/artist-details/artist-details.component').then(m => m.ArtistDetailsComponent),
+      },
+      {
+        path: '**',
+        redirectTo: 'home',
+      },
+    ],
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
