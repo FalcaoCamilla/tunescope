fb089989e7f56f13cf4d78dfcca7180a04a94b18
Camilla Falcão <millafalcao.acad@gmail.com>
2025-05-03 14:22:40 -0300

    feat: create ArtistsComponent with pagination and search functionality

---
+++ src/app/pages/artists/artists.component.ts
@@ -0,0 +1,51 @@
+import { CommonModule } from '@angular/common';
+import { Component, DestroyRef, Injector, OnInit, inject, runInInjectionContext } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Artist } from '@shared/models';
+import { CardComponent } from '@shared/components/card/card.component';
+import { PaginationComponent } from '@shared/components/pagination/pagination.component';
+import { SearchService } from '@shared/services/search.service';
+import { PaginatedListService } from '@shared/services/paginated-list.service';
+import { ListHeaderComponent } from '@shared/components/list-header/list-header.component';
+
+@Component({
+  selector: 'app-artists',
+  standalone: true,
+  imports: [CommonModule, CardComponent, ListHeaderComponent, PaginationComponent],
+  templateUrl: './artists.component.html',
+  styleUrls: ['./artists.component.scss'],
+})
+export class ArtistsComponent implements OnInit {
+  private searchService = inject(SearchService);
+  private paginatedListService = inject(PaginatedListService<Artist>);
+  destroy = inject(DestroyRef);
+  inject = inject(Injector);
+
+  displayedArtists = this.paginatedListService.displayedItems;
+  currentPage = this.paginatedListService.currentPage;
+  totalPages = this.paginatedListService.totalPages;
+
+  ngOnInit(): void {
+    this.listenerSearchTerm();
+  }
+
+  private listenerSearchTerm(): void {
+    runInInjectionContext(this.inject, () => {
+      this.searchService.searchTerm$
+        .pipe(takeUntilDestroyed(this.destroy))
+        .subscribe((searchTerm: string) => {
+          this.paginatedListService.resetParams();
+          this.paginatedListService.setSearchTerm(searchTerm.trim() !== '' ? searchTerm : 'artist');
+          this.paginatedListService.loadNextPage('artist');
+        });
+    })
+  }
+
+  loadNextPage(): void {
+    this.paginatedListService.loadNextPage('artist');
+  }
+
+  loadPreviousPage(): void {
+    this.paginatedListService.loadPreviousPage('artist');
+  }
+}
