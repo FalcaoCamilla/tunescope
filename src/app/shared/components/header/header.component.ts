f6d66bacd805658fb3c2c4774d68e277499bd669
Camilla Falcão <millafalcao.acad@gmail.com>
2025-05-04 00:54:33 -0300

    feat: implement back button functionality in header component

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, Location } from '@angular/common';
 import { SearchService } from '@shared/services/search.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -10,8 +11,25 @@ import { SearchService } from '@shared/services/search.service';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-  searchService = inject(SearchService);
+  showBackButton: boolean = false;
+
+  constructor(
+    private location: Location,
+    private searchService: SearchService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.router.events.subscribe(() => {
+      const childRoute = this.route.firstChild;
+      this.showBackButton = !!childRoute?.snapshot.paramMap.get('id');
+    });
+  }
+
   onSearch(searchTerm: string) {
     this.searchService.updateSearchTerm(searchTerm);
+  }
+
+  goBack() {
+    this.location.back();
   }
 }
