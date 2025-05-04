069c36cd15b18d5c9c9491d1c8fd7958ca21a9d2
Camilla Falcão <millafalcao.acad@gmail.com>
2025-05-04 00:52:21 -0300

    feat: refactor ApiService methods to use ListParams for improved parameter handling and new method getListById

--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -1,10 +1,11 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { listType } from '@shared/models';
+import { Album, listType } from '@shared/models';
 import { ApiResponse } from '@shared/models/api-response';
 import { environment } from '@environments/environment';
 import { map, Observable } from 'rxjs';
 import { IApiService } from '@shared/interfaces/api';
+import { ListParams } from '@shared/models/list-params';
 
 @Injectable({
   providedIn: 'root'
@@ -13,15 +14,28 @@ export class ApiService implements IApiService {
   http = inject(HttpClient);
   private readonly apiUrl = environment.apiUrl;
 
-  getListByName<T>(name: string, type: listType, limit: number, offset: number): Observable<ApiResponse<T>[listType]> {
+  getItemById<T>(id: string, itemType: listType): Observable<T> {
+    return this.http.get<T>(`${this.apiUrl}/${itemType}s/${id}`);
+  }
+
+  getListByName<T>(listParams: ListParams): Observable<{[key: string]: ApiResponse<T>}[listType]> {
     const params = new HttpParams()
-      .set('q', name)
-      .set('type', type)
-      .set('limit', limit.toString())
-      .set('offset', offset.toString());
+      .set('q', listParams.searchTerm ?? listParams.listType)
+      .set('type', listParams.listType)
+      .set('limit', listParams.limit.toString())
+      .set('offset', listParams.offset.toString());
   
-    return this.http.get<ApiResponse<T>>(`${this.apiUrl}/search`, { params }).pipe(
-      map((response) => response[type+'s'])
+    return this.http.get<{[key: string]: ApiResponse<T>}>(`${this.apiUrl}/search`, { params }).pipe(
+      map((response) => response[listParams.listType+'s'])
     );
+  }
+
+  //artists-albums e albums-tracks
+  getListById<T>(listParams: ListParams): Observable<ApiResponse<T>> {
+    const params = new HttpParams()
+      .set('limit', listParams.limit.toString())
+      .set('offset', listParams.offset.toString());
+    
+    return this.http.get<ApiResponse<T>>(`${this.apiUrl}/${listParams.itemType}s/${listParams.itemId}/${listParams.listType}s`, { params });
   }
 }
