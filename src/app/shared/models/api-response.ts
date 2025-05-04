fd31e63db4879e608d7f0a4524ede0383ae5a728
Camilla Falcão <millafalcao.acad@gmail.com>
2025-05-04 00:49:24 -0300

    refactor: refactor api-response type

--- src/app/shared/models/api-response.ts
+++ src/app/shared/models/api-response.ts
@@ -1,11 +1,9 @@
 export type ApiResponse<T> = {
-  [key: string]: {
-    items: T[];
-    href: string;
-    limit: number;
-    next: string | null;
-    offset: number;
-    previous: string | null;
-    total: number;
-  } 
+  items: T[];
+  href: string;
+  limit: number;
+  next: string | null;
+  offset: number;
+  previous: string | null;
+  total: number;
 }
