3be74e81189c6b9254846ca1e307c322601985dd
Camilla Falcão <millafalcao.acad@gmail.com>
2025-05-04 17:12:01 -0300

    feat: add production environment configuration for Spotify API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   authUrl: "https://accounts.spotify.com/api/token",
   apiUrl: "https://api.spotify.com/v1",
   wikipediaApi: "https://en.wikipedia.org/api/rest_v1",
@@ -7,6 +7,6 @@ export const environment = {
     id: "bb1af51000784f2385e85f466ccfff5b",
     secret: "e097da451d0546178c0bb36567ccdad6"
   },
-  redirectUri: "https://tunescope-jsbyvgfvj-falcaocamillas-projects.vercel.app/callback",
+  redirectUri: "https://tunescope.vercel.app/callback",
   accountsSpotifyUrl: "https://accounts.spotify.com/",
 };
