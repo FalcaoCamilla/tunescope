fcc1034f2e4b0ca3a10055bc48d5090a493ed250
Camilla Falcão <millafalcao.acad@gmail.com>
2025-05-04 00:53:34 -0300

    feat: add new api methods

--- src/app/shared/interfaces/api.ts
+++ src/app/shared/interfaces/api.ts
@@ -1,12 +1,10 @@
 import { Observable } from 'rxjs';
 import { listType } from '@shared/models';
 import { ApiResponse } from '@shared/models/api-response';
+import { ListParams } from '@shared/models/list-params';
 
 export interface IApiService {
-  getListByName<T>(
-    name: string,
-    type: listType,
-    limit: number,
-    offset: number
-  ): Observable<ApiResponse<T>[listType]>;
+  getListByName<T>(listParams: ListParams): Observable<{[key: string]: ApiResponse<T>}[listType]>
+  getListById<T>(listParams: ListParams): Observable<ApiResponse<T>>
+  getItemById<T>(id: string, type: listType): Observable<T>
 }
