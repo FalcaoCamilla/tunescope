7b9029326c7a0b2fdbeec15435d70d3ce4b92341
Camilla Falcão <millafalcao.acad@gmail.com>
2025-05-04 13:50:33 -0300

    feat: add wikipediaApiUrl environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: false,
   authUrl: "https://accounts.spotify.com/api/token",
   apiUrl: "https://api.spotify.com/v1",
+  wikipediaApi: "https://en.wikipedia.org/api/rest_v1",
   client: {
     id: "bb1af51000784f2385e85f466ccfff5b",
     secret: "e097da451d0546178c0bb36567ccdad6"
