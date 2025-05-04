2b02ba88e59e0a00539edcd59791c1617af026dc
Camilla Falcão <millafalcao.acad@gmail.com>
2025-05-04 16:53:12 -0300

    feat: add IWikipediaService interface

--- src/app/shared/services/wikipedia.service.ts
+++ src/app/shared/services/wikipedia.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '@environments/environment';
-import { IWikipediaService } from '@shared/interfaces/wikipedia';
+import { IWikipediaService } from '@shared/interfaces/wikipedia.interface';
 import { map, Observable } from 'rxjs';
 
 @Injectable({
