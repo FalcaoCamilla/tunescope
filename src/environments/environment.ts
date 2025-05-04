0c74e9ffdc605ae5f06a5a48141b97e995e3f1e4
Camilla Falcão <millafalcao.acad@gmail.com>
2025-05-04 19:51:30 -0300

    fix: update redirectUri to the correct production URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,6 @@ export const environment = {
   wikipediaApi: "https://en.wikipedia.org/api/rest_v1",
   clientId: "bb1af51000784f2385e85f466ccfff5b",
   clientSecret: "e097da451d0546178c0bb36567ccdad6",
-  redirectUri: "https://tunescope-jsbyvgfvj-falcaocamillas-projects.vercel.app/callback",
+  redirectUri: "https://tunescope.vercel.app/callback",
   accountsSpotifyUrl: "https://accounts.spotify.com/",
 };
