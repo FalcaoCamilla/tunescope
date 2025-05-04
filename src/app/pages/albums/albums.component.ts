94b420718619e21f8846a3c0b85c091c5531da9d
Camilla Falcão <millafalcao.acad@gmail.com>
2025-05-04 00:58:18 -0300

    feat: add skeleton loading for albums and handle artistId in pagination

--- src/app/pages/albums/albums.component.ts
+++ src/app/pages/albums/albums.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component, DestroyRef, inject, Injector, runInInjectionContext } from '@angular/core';
+import { Component, DestroyRef, inject, Injector, Input, runInInjectionContext } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { CardComponent } from '@shared/components/card/card.component';
 import { ListHeaderComponent } from '@shared/components/list-header/list-header.component';
 import { PaginationComponent } from '@shared/components/pagination/pagination.component';
+import { SkeletonCardComponent } from '@shared/components/skeleton-card/skeleton-card.component';
 import { Album } from '@shared/models/album';
 import { PaginatedListService } from '@shared/services/paginated-list.service';
 import { SearchService } from '@shared/services/search.service';
@@ -11,7 +12,7 @@ import { SearchService } from '@shared/services/search.service';
 @Component({
   selector: 'app-albums',
   standalone: true,
-  imports: [CommonModule, CardComponent, ListHeaderComponent, PaginationComponent],
+  imports: [CommonModule, CardComponent, ListHeaderComponent, PaginationComponent, SkeletonCardComponent],
   templateUrl: './albums.component.html',
   styleUrls: ['./albums.component.scss']
 })
@@ -21,12 +22,22 @@ private searchService = inject(SearchService);
   destroy = inject(DestroyRef);
   inject = inject(Injector);
 
+  @Input() artistId: string | null = null;
+
   displayedAlbums = this.paginatedListService.displayedItems;
   currentPage = this.paginatedListService.currentPage;
   totalPages = this.paginatedListService.totalPages;
+  loading = this.paginatedListService.loading;
+
+  skeletonArray = Array(12);
 
   ngOnInit(): void {
-    this.listenerSearchTerm();
+    this.paginatedListService.resetParams();
+    if(this.artistId) {
+      this.paginatedListService.loadNextPage('artist', this.artistId);
+    } else {
+      this.listenerSearchTerm()
+    };
   }
 
   private listenerSearchTerm(): void {
@@ -42,10 +53,13 @@ private searchService = inject(SearchService);
   }
 
   loadNextPage(): void {
+    if(this.artistId) {
+      return this.paginatedListService.loadNextPage('artist', this.artistId);
+    }
     this.paginatedListService.loadNextPage('album');
   }
 
   loadPreviousPage(): void {
-    this.paginatedListService.loadPreviousPage('album');
+    this.paginatedListService.loadPreviousPage();
   }
 }
