7dd64669109e85b7a9fac3e9f87b38523596367b
Camilla Falcão <millafalcao.acad@gmail.com>
2025-05-04 13:51:28 -0300

    feat: refactor getArtist method to use RxJS operators for improved handling of artist data and biography fetching

--- src/app/pages/artists/artist-details/artist-details.component.ts
+++ src/app/pages/artists/artist-details/artist-details.component.ts
@@ -5,6 +5,8 @@ import { ApiService } from '@shared/services/api.service';
 import { Artist } from '@shared/models';
 import { AlbumsComponent } from '@pages/albums/albums.component';
 import { CardDetailsComponent } from '@shared/components/card-details/card-details.component';
+import { WikipediaService } from '@shared/services/wikipedia.service';
+import { finalize, of, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-artist-details',
@@ -15,6 +17,7 @@ import { CardDetailsComponent } from '@shared/components/card-details/card-detai
 })
 export class ArtistDetailsComponent implements OnInit {
   apiService = inject(ApiService);
+  wikipediaService = inject(WikipediaService);
   route = inject(ActivatedRoute);
   protected artist: Artist = {} as Artist;
 
@@ -23,12 +26,30 @@ export class ArtistDetailsComponent implements OnInit {
   }
 
   getArtist() {
-    this.route.paramMap.subscribe((params) => {
-      const id = params.get('id');
-      if (id) {
-        this.apiService.getItemById<Artist>(id, 'artist').subscribe((res) => {
-          this.artist = res;
-        });
+    this.route.paramMap
+    .pipe(
+      switchMap((params) => {
+        const id = params.get('id');
+        if(id) return this.apiService.getItemById<Artist>(id, 'artist');
+        return of(null); //retorna um observable vazio se o id não existir
+      }),
+      switchMap((artist) => {
+        if (artist) {
+          this.artist = artist;
+          return this.wikipediaService.getBiography(artist.name);
+        }
+        return of(''); //retorna um observable vazio se o artista não existir
+      }),
+      finalize(() => {
+        if(!this.artist.biography) this.artist.biography = 'Loading biography...';
+      })
+    )
+    .subscribe({
+      next: (biography) => {
+        if(biography) this.artist.biography = biography;
+      },
+      error: () => {
+        this.artist.biography = 'Sorry, I could not find any information about this artist.';
       }
     });
   }
