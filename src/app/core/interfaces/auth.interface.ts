f4012c13de9c02dbe42534fd30c537fd859d57a2
Camilla Falcão <millafalcao.acad@gmail.com>
2025-05-04 19:58:29 -0300

    refactor: update IAuthService interface to include new methods for Spotify authentication

--- src/app/core/interfaces/auth.interface.ts
+++ src/app/core/interfaces/auth.interface.ts
@@ -1,7 +1,11 @@
-import { UserData } from "@core/models/user";
+import { Observable } from 'rxjs';
+import { AccessDataResponse, UserData } from '@core/models';
 
 export interface IAuthService {
   isAuthenticated(): boolean;
   login(data: UserData): void;
   logout(): void;
+  redirectToSpotifyAuth(): void;
+  exchangeCodeForToken(code: string): Observable<AccessDataResponse>;
+  saveAuthData(data: AccessDataResponse): void;
 }
