2108719e1b3b210807f112cdaf6270aba7b28cd4
Camilla Falcão <millafalcao.acad@gmail.com>
2025-05-04 04:44:31 -0300

    feat: add album details component

---
+++ src/app/pages/albums/album-details/album-details.component.ts
@@ -0,0 +1,35 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardDetailsComponent } from '@shared/components/card-details/card-details.component';
+import { ActivatedRoute } from '@angular/router';
+import { Album } from '@shared/models';
+import { ApiService } from '@shared/services/api.service';
+import { TracksComponent } from '@pages/tracks/tracks.component';
+
+@Component({
+  selector: 'app-album-details',
+  standalone: true,
+  imports: [CommonModule, CardDetailsComponent, TracksComponent],
+  templateUrl: './album-details.component.html',
+  styleUrls: ['./album-details.component.scss']
+})
+export class AlbumDetailsComponent {
+  apiService = inject(ApiService);
+  route = inject(ActivatedRoute);
+  protected album: Album = {} as Album;
+
+  ngOnInit() {
+    this.getAlbum();
+  }
+
+  getAlbum() {
+    this.route.paramMap.subscribe((params) => {
+      const id = params.get('id');
+      if (id) {
+        this.apiService.getItemById<Album>(id, 'album').subscribe((res) => {
+          this.album = res;
+        });
+      }
+    });
+  }
+}
