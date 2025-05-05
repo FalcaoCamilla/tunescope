a3cf5de7508f62f2b5ab71ec40b18f5112eeed77
Camilla Falcão <millafalcao.acad@gmail.com>
2025-05-04 23:52:08 -0300

    feat: implement user service with spotify user management and favorites retrieval

--- src/app/shared/interfaces/user.interface.ts
+++ src/app/shared/interfaces/user.interface.ts
@@ -1,6 +1,14 @@
+import { ApiResponse, listType } from '@shared/models';
+import { ListParams } from '@shared/models/list-params';
 import { User } from '@shared/models/user';
 import { Observable } from 'rxjs';
 
 export interface IUserService {
-  getUser(): Observable<User>;
+  setSpotifyUser(): void; //busca e define os detalhes do usuário autenticado pelo Spotify
+  setUser(user: Partial<User>): void; //define manualmente os dados do usuário
+  clearUser(): void; //limpa os dados do usuário
+  getCurrentUserDetails(): Observable<User>; //busca os detalhes do usuário atual
+  getCurrentUserFavorites<T>(
+    listParams: Pick<ListParams, 'listType' | 'limit'>
+  ): Observable<{ [key: string]: ApiResponse<T> }[listType]>; //busca os favoritos do usuário
 }
---
+++ src/app/shared/services/user.service.ts
@@ -0,0 +1,54 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '@environments/environment';
+import { IUserService } from '@shared/interfaces/user.interface';
+import { ApiResponse, listType } from '@shared/models';
+import { ListParams } from '@shared/models/list-params';
+import { User } from '@shared/models/user';
+import { BehaviorSubject, map, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService implements IUserService{
+  http = inject(HttpClient);
+  private readonly apiUrl = environment.apiUrl;
+
+  //uso de behavior para garantir que ao buscar detalhes do usuário o listener seja atualizado
+  private userSubject = new BehaviorSubject<Partial<User> | null>(null);
+  user$ = this.userSubject.asObservable();
+
+  setSpotifyUser(): void {
+    this.getCurrentUserDetails().subscribe({
+      next: (userDetails) => this.setUser(userDetails),
+      error: (err) => {
+        console.error('Erro ao buscar detalhes do usuário Spotify:', err);
+        this.userSubject.next(null);
+      }
+    });
+  }
+
+  setUser(user: Partial<User>): void {
+    localStorage.setItem('userData', JSON.stringify(user));
+    this.userSubject.next(user);
+  }
+
+  clearUser(): void {
+    localStorage.removeItem('userData');
+    this.userSubject.next(null);
+  }
+
+  getCurrentUserDetails(): Observable<User> {
+    return this.http.get<User>(`${this.apiUrl}/me`);
+  }
+
+  getCurrentUserFavorites<T>(listParams: Pick<ListParams, 'listType'| 'limit'>): Observable<{[key: string]: ApiResponse<T>}[listType]> {
+    const params = new HttpParams()
+      .set('limit', listParams.limit.toString())
+      .set('type', listParams.listType);
+
+    return this.http.get<{[key: string]: ApiResponse<T>}>(`${this.apiUrl}/me/following`, { params }).pipe(
+      map((response) => response[listParams.listType+'s'])
+    );
+  }
+}
