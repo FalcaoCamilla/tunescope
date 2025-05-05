469d2b73313a9b687801c358b19bfff71cc6de60
Camilla Falcão <millafalcao.acad@gmail.com>
2025-05-05 00:18:19 -0300

    fix: fix login type key in auth guard

--- src/app/core/guards/spotify-auth.guard.ts
+++ src/app/core/guards/spotify-auth.guard.ts
@@ -5,7 +5,7 @@ export const spotifyAuthGuard: CanActivateFn = (route, state) => {
   const AUTH_KEY = 'authData';
   const router = inject(Router);
   const authData = localStorage.getItem(AUTH_KEY);
-  const loginType = authData ? JSON.parse(authData).loginType : null;
+  const loginType = authData ? JSON.parse(authData).login_type : null;
 
   if (loginType === 'spotify') {
     return true;
