ce0006ea40c26c4a826f82e95974d94baa86580c
Camilla Falcão <millafalcao.acad@gmail.com>
2025-05-04 23:53:31 -0300

    feat: add user display in header component

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,27 +1,43 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { CommonModule, Location } from '@angular/common';
 import { SearchService } from '@shared/services/search.service';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { UserService } from '@shared/services/user.service';
+import { User } from '@shared/models/user';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   detailsRoute = signal(false);
-
+  user: Partial<User> | null = null;
   constructor(
     private location: Location,
-    private searchService: SearchService,
     private route: ActivatedRoute,
-    private router: Router
-  ) {
+    private router: Router,
+    private searchService: SearchService,
+    private userService: UserService
+  ) {}
+
+  ngOnInit(): void {
+    this.listenerRouterEvents();
+    this.listenerUser();
+  }
+
+  listenerRouterEvents() {
     this.router.events.subscribe(() => {
       const childRoute = this.route.firstChild;
       this.detailsRoute.set(!!childRoute?.snapshot.paramMap.get('id'));
+    });
+  }
+
+  listenerUser() {
+    this.userService.user$.subscribe((user) => {
+      this.user = user;
     });
   }
 
